0cb011f7d6140fbb87b7b6962d22e9e348eb4df7
Mawaddah <itzolda@gmail.com>
2021-05-24 17:26:25 +0100

    added navbar icons

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import 'semantic-ui-css/semantic.min.css'
-import{Button, Container} from "semantic-ui-react";
+import './index.css';
+
+import{Button, Container, Menu, Icon} from "semantic-ui-react";
 
 
 function App() {
   return (
-    <Container>
-     <Button> the flippin button </Button>
-      </Container>
+  
+      <Menu>
+        <Container className='navBar'>
+        <Menu.Item><Icon fitted name='sidebar' size='large'/></Menu.Item>
+        <Menu.Item><Icon fitted name='home' size='large'/></Menu.Item>
+        <Menu.Item><Icon fitted name='user circle' size='large'/></Menu.Item>
+    </Container>
+    </Menu>
+   
   );
 }
 export default App;
